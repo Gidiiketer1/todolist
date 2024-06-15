59621f2635d699f4b0161a91d1cab8c9b07adc69
Gidiiketer2 <gideonkipketer52@gmail.com>
2024-06-15 20:09:10 +0300

    upload files

--- todo-list-web-app/backend/config.py
+++ todo-list-web-app/backend/config.py
@@ -2,5 +2,11 @@
 
 class Config:
     SECRET_KEY = b'\x9c\x0bJ\xa7\xe9\xd8\xa1\x18\xe8\xddA\xf6\xa8\x9e\xd2\xf8\x90\xdc\xbd\xd5\xb6\x7f\x07'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/todo_list_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dev-user17:16153371G.4.L@localhost/todo_list_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+
+
+
--- todo-list-web-app/backend/routes/index.py
+++ todo-list-web-app/backend/routes/index.py
@@ -5,12 +5,5 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
-<<<<<<< HEAD
-    return render_template('index.html', current_route='index')
-=======
     return render_template('index.html', current_route='index')
 
-@index_bp.route('/login')
-def login():
-    return render_template('login.html', current_route='login')
->>>>>>> 4e9a4a03fae338c270b479cccd315b12b4463eb3
