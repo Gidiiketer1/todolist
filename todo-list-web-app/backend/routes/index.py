d607af1cc16370b029b78ce59a1918edcafc8627
Gidiiketer2 <gideonkipketer52@gmail.com>
2024-06-15 19:13:45 +0300

    Upload Files

--- todo-list-web-app/backend/routes/index.py
+++ todo-list-web-app/backend/routes/index.py
@@ -6,16 +6,3 @@ index_bp = Blueprint('index', __name__)
 @index_bp.route('/')
 def index():
     return render_template('index.html', current_route='index')
-
-# Create the Flask application
-app = Flask(__name__)
-
-@app.route('/login')
-def login():
-    return render_template('login.html', current_route='login')
-
-# Register the blueprint
-app.register_blueprint(index_bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
